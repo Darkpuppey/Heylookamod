11ed8d379b7253838f5720252871065a3ceb319d
Darkpuppey <darkpuppey123@gmail.com>
2019-03-07 16:06:53 +1100

    Add files via upload

--- Items/Peashooter.cs
+++ Items/Peashooter.cs
@@ -28,15 +28,27 @@ namespace Heylookamod.Items
             item.reuseDelay = 1;
             item.useStyle = 5;
             item.noMelee = true; 
-            item.knockBack = 2;
+            item.knockBack = 1;
             item.value = 10000;
             item.rare = 2;
             item.UseSound = mod.GetLegacySoundSlot(Terraria.ModLoader.SoundType.Custom, "Sounds/FloweyHurt");
-            item.autoReuse = true;
+            item.autoReuse = false;
             item.shoot = mod.ProjectileType("Pea");
-            item.shootSpeed = 10f;
+            item.shootSpeed = 7f;
         }
-
+        public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
+        {
+            int numberProjectiles = 5 + Main.rand.Next(7); // 4 or 5 shots
+            for (int i = 0; i < numberProjectiles; i++)
+            {
+                Vector2 perturbedSpeed = new Vector2(speedX, speedY).RotatedByRandom(MathHelper.ToRadians(15)); // 30 degree spread.
+                                                                                                                // If you want to randomize the speed to stagger the projectiles
+                float scale = 1f - (Main.rand.NextFloat() * .10f);
+                perturbedSpeed = perturbedSpeed * scale; 
+                Projectile.NewProjectile(position.X, position.Y, perturbedSpeed.X, perturbedSpeed.Y, type, damage, knockBack, player.whoAmI);
+            }
+            return false; // return false because we don't want tmodloader to shoot projectile
+        }
         public override void AddRecipes()
         {
             ModRecipe recipe = new ModRecipe(mod);
